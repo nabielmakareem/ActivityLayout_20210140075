b1952d9cd4dce50ad2e50699ebca3eef077f6f48
nabielmakareem <nabielmakareem10@gmail.com>
2023-10-05 21:43:37 +0700

    Finish

---
+++ app/src/main/java/com/example/nyobaaja/frame_activity.kt
@@ -0,0 +1,11 @@
+package com.example.nyobaaja
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class frame_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame)
+    }
+}
